4543df6e56e1fb2300d2510ba9740ecc6f45a4cc
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2024-07-05 10:39:35 +0300

    Add files via upload

---
+++ multi-stalkerpro-1.2-3.9-arm.sh
@@ -0,0 +1,28 @@
+echo
+rm -r /usr/lib/enigma2/python/Plugins/Extensions/XKlass
+wait
+#!/bin/sh
+#
+
+wget -O /tmp/multi-stalkerpro-1.2-3.9-arm.tar.gz "https://raw.githubusercontent.com/Ham-ahmed/MStalkerpro/main/multi-stalkerpro-1.2-3.9-arm.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+rm -r /tmp/multi-stalkerpro-1.2-3.9-arm.tar.gz
+sleep 2;
+echo "" 
+echo "" 
+echo "*********************************************************"
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "*********************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo '================================================='
+###########################################                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "*********************************************************"
+wait
+killall -9 enigma2
+exit 0
